b1f3035716c66ff9de2bc06b4d7e25667ea7970e
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-25 08:36:18 +0000

    done

--- 06_keyboard_Magic/javascript.js
+++ 06_keyboard_Magic/javascript.js
@@ -1,3 +1,20 @@
-const screen = document.querySelector('#container');
-console.log(screen);
-screen.addEventListner()
+const node = document.getElementById('container');
+window.addEventListener('keydown',(e)=>{
+    let res = document.getElementById('table');
+    res.innerHTML=`
+    <div>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th>
+      <th>code</th>
+    </tr>
+    <tr>
+      <td>${e.key}</td>
+      <td> ${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+   </table> 
+    </div>
+    `
+},false)
